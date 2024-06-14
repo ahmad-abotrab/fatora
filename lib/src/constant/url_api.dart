a73392c17d7cc28e0a14c47907fc0090df2ac1d1
AhmadAbotrab <abotrab.ahmad3099@gmail.com>
2024-06-14 11:25:21 +0300

    change ip

--- lib/src/constant/url_api.dart
+++ lib/src/constant/url_api.dart
@@ -1,5 +1,6 @@
 class URLApi {
-  static String baseUrl = 'http://77.44.176.222:8080/';
+  // static String baseUrl = 'http://77.44.176.222:8080/';
+  static String baseUrl = 'http://185.194.126.157/';
   // static String baseUrl = 'http://127.0.0.1:8000/';
   static String getAllReceipts = 'api/allReceipt';
   static const String addNewIdChar = 'api/getLocalCharID';
@@ -7,12 +8,13 @@ class URLApi {
   static String addNewReceipt = 'api/addReceipt';
   static String getLastId = 'api/lastRecord';
   static String getReceiptsBetweenRangeDate = 'api/filterByDate';
-  static String  createNewLocalCharID= 'api/createNewLocalCharID';
-  static String addLocalIdToServer= 'api/addLocalIdToServer';
-  static String  updateLocalNumId = 'api/updateLocalNumId';
+  static String createNewLocalCharID = 'api/createNewLocalCharID';
+  static String addLocalIdToServer = 'api/addLocalIdToServer';
+  static String updateLocalNumId = 'api/updateLocalNumId';
   static String updateStatusOfWhatsAppSend = 'api/updateStatusOfWhatsAppSend';
   static String getBeforeLocalID = 'api/getBeforeLocalID';
   static int port = 8080;
+
   ///editing
 
 }
